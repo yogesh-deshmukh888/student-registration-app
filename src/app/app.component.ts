46699d7c3c72cc8d48f171d3a870f0e39a600814
Yogesh-deshmukh888 <deshmukhy8888@gmail.com>
2025-04-02 14:10:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'student-registration-app';
+  students: any[] = [
+    {
+      name: 'Yogesh',
+      age: 24,
+      email: 'yogesh@example.com',
+      course: 'Computer Science'
+    },
+    {
+      name: 'Rudraksha',
+      age: 23,
+      email: 'rudraksha@example.com',
+      course: 'Mechanical Engineering'
+    }
+  ];
+
+  student: any = { name: '', age: '', email: '', course: '' };
+
+  registerStudent() {
+    if (this.student.name && this.student.age && this.student.email && this.student.course) {
+      this.students.push({ ...this.student });
+      this.resetForm();
+    }
+  }
+
+  deleteStudent(index: number) {
+    this.students.splice(index, 1);
+  }
+
+ 
+  resetForm() {
+    this.student = { name: '', age: '', email: '', course: '' };
+  }
 }
